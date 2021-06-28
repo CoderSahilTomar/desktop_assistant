03b5ff819e94283a1cef783434120b89fca5ea12
CoderSahilTomar <tomarsahil152@gmail.com>
2021-06-28 11:23:05 +0530

    changed wishMe to greetMe

--- jarvis.py
+++ jarvis.py
@@ -1,5 +1,6 @@
 import pyttsx3 as tts
 import datetime
+import speech_recognition as sr
 
 
 engine = tts.init("sapi5")
@@ -7,16 +8,23 @@ voices = engine.getProperty('voices')
 engine.setProperty('voices',voices[0].id)
 
 def speak(sound):
+    '''Makes the assistant speak out stuff'''
     engine.say(sound)
     print(sound)
     engine.runAndWait()
 
-def wishMe():
+def greetMe():
+    '''greets the user'''
     hour = datetime.datetime.now().hour
     if hour>=0 and hour<12: speak("Good Morning")
     elif hour>=12 and hour<18: speak("Good Afternoon")
     else: speak("Good Evening")
     speak("I am your Desktop Assistant, how may I help you?")
+
+
+
+
+
 
 if __name__ == '__main__':
     wishMe()
